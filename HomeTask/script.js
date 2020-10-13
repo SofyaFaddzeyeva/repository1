b2decc519adb06e3ac9acc7c734f46f748b15eae
Sonya <sonyafadd@gmail.com>
2020-10-13 22:15:10 +0300

    HomeTask/

---
+++ HomeTask/script.js
@@ -0,0 +1,32 @@
+function functionTotal(a, b) {
+    if (a + b < 100) {
+        return true
+    }
+    else {
+        return false
+    }
+} 
+var result = functionTotal(40, 50);
+console.log(result);
+
+
+function functionTotalSeconds(hours, minutes) {
+    return result = minutes*60 + hours*3600;
+}
+var result = functionTotalSeconds(1, 3);
+console.log(result);
+
+
+function functionPoints(win, defeat, draw) {
+    return result = win*3 + defeat*1 + draw*0;
+}
+var result = functionPoints(2, 1, 1);
+console.log(result);
+
+
+function functionMakePair(a, b) {
+var array = [a, b];
+return array;
+}
+var result = functionMakePair(1, 3);
+console.log(result);
